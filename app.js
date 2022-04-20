6bed03c5b333994306035ffdf723de7db370962c
eidanfili <anning3232@gmail.com>
2022-04-19 19:14:58 -0600

    second commit with total counting(still has a bug to fix)

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+var total = 0;
+var populateDiv = document.querySelector(".selected-population");
+
 function addedToCart(className) {
   var selectedClass = document.querySelector(`.${className}`);
+  console.log(selectedClass.childNodes);
   var selectedText = selectedClass.childNodes[3].innerHTML;
+  var selectedPrice = selectedClass.childNodes[7].innerHTML;
   var showSelection = document.createElement("div");
   var textForSelection = document.createTextNode(selectedText);
   showSelection.appendChild(textForSelection);
-  var populateDiv = document.querySelector(".selected-population");
   populateDiv.appendChild(showSelection);
+  total = total + Number(selectedPrice);
+}
+
+function showTotal() {
+  var showTotal = document.createElement("div");
+  var totalToShow = document.createTextNode(total);
+  showTotal.appendChild(totalToShow);
+  populateDiv.appendChild(showTotal);
 }
