e89e582397ad7f9e649e06f26ca9b3fa5679e6b0
eidanfili <anning3232@gmail.com>
2022-04-21 20:07:53 -0600

    added item removal

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 var total = 0;
 var populateDiv = document.querySelector(".selected-population");
 
-function addedToCart(className) {
+var largeToy = 1;
+var smallToy = 2;
+
+function itemAmmountVerification(size) {
+  var largeDiv = document.querySelector(".large");
+  console.log(largeDiv);
+  var smallDiv = document.querySelectorAll(".small");
+  console.log(smallDiv);
+
+  if (size === "large") {
+    largeToy = largeToy - 1;
+    if (largeToy === 0) {
+      largeDiv.classList.add("hidden");
+    }
+  } else if (size === "small") {
+    smallToy = smallToy - 1;
+    if (smallToy === 0) {
+      smallDiv.forEach((item) => {
+        item.classList.add("hidden");
+      });
+    }
+    console.log(smallToy);
+  }
+}
+
+function addedToCart(className, itemSize) {
   var selectedClass = document.querySelector(`.${className}`);
-  console.log(selectedClass.childNodes);
+  // console.log(selectedClass.childNodes);
   var selectedText = selectedClass.childNodes[3].innerHTML;
   var selectedPrice = selectedClass.childNodes[7].innerHTML;
   var showSelection = document.createElement("div");
@@ -11,6 +36,13 @@ function addedToCart(className) {
   showSelection.appendChild(textForSelection);
   populateDiv.appendChild(showSelection);
   total = total + Number(selectedPrice);
+  itemAmmountVerification(itemSize);
+  // waitressAlert();
+  showTotal();
+}
+
+function waitressAlert() {
+  alert("nice choice");
 }
 
 function showTotal() {
